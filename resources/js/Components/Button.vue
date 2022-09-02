e1a4a0bf739280854d19f9c129ee8e4a13df9473
Lyte Onyema <lyte.onyema@gmail.com>
2022-09-02 22:16:50 +0100

    project color set up

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-secondary-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-secondary-700 active:bg-secondary-900 focus:outline-none focus:border-secondary-900 focus:shadow-outline-gray transition ease-in-out duration-150">
         <slot />
     </button>
 </template>
