f8d4b28160688bd0d762f7090481a46bbf057e32
Lyte Onyema <lyte.onyema@gmail.com>
2022-09-03 09:41:47 +0100

    set up role migration and seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = ['Product Designer', 'Backend Developer', 'Frontend Developer', 'Mobile Developer', 'Quality Assurance'];
+
+        collect($roles)->each(function ($value) {
+            Role::query()->create([
+                'name' => $value,
+                'slug' => Str::slug($value)
+            ]);
+        });
+    }
+}
